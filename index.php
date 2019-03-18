a318ad782448023ee18fded40a203fe3132d8e0a
Mert71 <31502152+Mert71@users.noreply.github.com>
2019-03-18 10:58:22 +0100

    Superhero works as intended.

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
 require 'Superhero.php';
+require 'Avenger.php';
+
+$spiderman =  new Superhero('Spider-Man', 'Male', 'Spiderfriends', 'With great power comes great responsibility!');
+
+echo'<pre>';
+print_r( $spiderman);
+
+$spiderman->sayOneliner();
+
+$thor = new Avenger('Thor', 'Male', 'For Asgard!');
+$thor->sayOneliner();
+
+print_r('<pre>' . $thor . '</pre>');
